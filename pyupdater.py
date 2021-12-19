7e363b2f890c9d6343473e1cade12913553863ff
sj <jenek46@ya.ru>
2021-12-19 22:15:32 +0300

    4

--- pyupdater.py
+++ pyupdater.py
@@ -1,12 +1,14 @@
 import subprocess, time
-from threading import Thread
+from threading import Thread, excepthook
 from os import name, system
 
 ulst = None
 
 def clear(): 
-    if name == 'nt': system('cls')
-    else: system('clear')
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
 
 def progress():
     global ulst
@@ -16,21 +18,27 @@ def progress():
                 clear()
                 print(f'Поиск обновлений {i}', flush=True)
                 time.sleep(0.5)
-            else: return 1
+            else: 
+                exit()
 
 def updater():
     global ulst
     ulst = subprocess.run('python -m pip list -o'.split(), capture_output=True, text=True).stdout
-    print(ulst)
-    ulst = ulst.split()
-    try:
-        if ulst == []: raise ValueError('Пустое значение')
-        for i in ulst[8::4]: subprocess.run(f'python -m pip install -U {i}'.split())
+    clear()
+    if ulst != '':
+        ulst = ulst.split()
+        for i in ulst[8::4]: 
+            subprocess.run(f'python -m pip install -U {i}'.split())
         print('Обновление пакетов завершено')
-    except ValueError: 
+    else:
         print('Нет обновлений')
+    ulst = ulst.split()
 
 th1 = Thread(target=progress, args=())
 th2 = Thread(target=updater, args=())
+
 th1.start()
 th2.start()
+
+th1.join()
+th2.join()
