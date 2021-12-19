bf4ada13ff1368a13139a580788b8d826e5e6643
sj <jenek46@ya.ru>
2021-12-19 23:30:57 +0300

    2 variant

--- pyupdater.py
+++ pyupdater.py
@@ -1,44 +1,25 @@
 import subprocess, time
 from os import system, name
-import sys
-k = 1
-ulst = None
 
-# def clear():                        # define the clear function
-#     if name == 'nt': system('cls')  # for windows
-#     else: system('clear')           # for mac and linux
+def clear():
+    if name == 'nt': system('cls')  # windows
+    else: system('clear')           # mac linux
 
-ulst = subprocess.Popen(args='python -m pip list -o'.split(), text=True, stdout=subprocess.PIPE)
-# one, two = out
-# print(out)
-while ulst.returncode == None:
-    ulst.poll()
-    time.sleep(1)
-    # try:
-    #     print(ulst)
-    #     # ulst = ulst.stdout
-    # except AttributeError:
-        # out = ulst.communicate(10)
-        # print(out)
-        # print(type(out))
-        # time.sleep(1)
 
-# while True: 
-#     for i in ["\\", "|", "/", "-"]:
-#         if ulst == None:
-#             # clear()
-#             print(f'Поиск обновлений {i}', flush=True)
-#             time.sleep(0.5)
-#         else:
-#             print(ulst, type(data))
-#             break
-
-# def updater():
-#     global ulst
-#     ulst = ulst.split()
-#     try:
-#         if ulst == []: raise ValueError('Пустое значение')
-#         for i in ulst[8::4]: subprocess.run(f'python -m pip install -U {i}'.split())
-#         print('Обновление пакетов завершено')
-#     except ValueError: 
-#         print('Нет обновлений')
+# proc = subprocess.Popen(args='python -m pip list -o'.split(), text=True, stdout=subprocess.PIPE)
+proc = subprocess.Popen(args='pip list -o'.split(), text=True, stdout=subprocess.PIPE)
+while proc.returncode == None:
+    for i in ["\\", "|", "/", "-"]:
+        proc.poll()
+        clear()
+        print(f'Поиск обновлений {i}')
+        time.sleep(0.5)
+    clear()
+outs, _ = proc.communicate()
+if outs != '':
+    outs = outs.split()
+    for i in outs[8::4]: 
+        subprocess.run(f'pip install {i}'.split())
+    print('Обновление пакетов завершено')
+else:
+    print('Нет обновлений')
