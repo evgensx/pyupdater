7015c2781c7708cea53d9e0dbd56b07224805509
sj <jenek46@ya.ru>
2021-12-19 23:34:06 +0300

    debug1

--- pyupdater.py
+++ pyupdater.py
@@ -5,9 +5,7 @@ def clear():
     if name == 'nt': system('cls')  # windows
     else: system('clear')           # mac linux
 
-
-# proc = subprocess.Popen(args='python -m pip list -o'.split(), text=True, stdout=subprocess.PIPE)
-proc = subprocess.Popen(args='pip list -o'.split(), text=True, stdout=subprocess.PIPE)
+proc = subprocess.Popen(args='python -m pip list -o'.split(), text=True, stdout=subprocess.PIPE)
 while proc.returncode == None:
     for i in ["\\", "|", "/", "-"]:
         proc.poll()
@@ -19,7 +17,7 @@ outs, _ = proc.communicate()
 if outs != '':
     outs = outs.split()
     for i in outs[8::4]: 
-        subprocess.run(f'pip install {i}'.split())
+        subprocess.run(f'python -m pip install -U {i}'.split())
     print('Обновление пакетов завершено')
 else:
     print('Нет обновлений')
