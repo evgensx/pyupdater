e7db0b7ac39e820c1c2991781d471c75e371aa8e
sj <jenek46@ya.ru>
2021-12-19 19:26:32 +0300

    2

--- pyupdater.py
+++ pyupdater.py
@@ -1,33 +1,44 @@
 import subprocess, time
-from threading import Thread
+# import os module  
+from os import system, name
 
+k = 0
 ulst = None
+data = None
 
-def dotter():
-    global ulst
-    k = 0
-    print('Поиск обновлений',end='')
-    while k < 10 and ulst == None:
-        print('.', end='', flush=True)
+def clear():                        # define the clear function
+    if name == 'nt': system('cls')  # for windows
+    else: system('clear')           # for mac and linux
+
+ulst = subprocess.Popen(args='python -m pip list -o'.split(), stdout=subprocess.PIPE)
+while True:
+    if ulst == None:
+        print(type(ulst))
         time.sleep(1)
-        k += 1
+    else:
+        break
+# while True: 
+#     for i in ["\\", "|", "/", "-"]:
+#         if ulst == None:
+#             # clear()
+#             print(f'Поиск обновлений {i}', flush=True)
+#             time.sleep(0.5)
+#         else:
+#             print(ulst, type(data))
+#             break
 
-def updater():
-    global ulst
-    ulst = subprocess.run('python -m pip list -o'.split(), capture_output=True, text=True).stdout
-    print(ulst)
-    ulst = ulst.split()
-    try:
-        if ulst == []: raise ValueError('Пустое значение')
-        for i in ulst[8::4]: subprocess.run(f'python -m pip install -U {i}'.split())
-        print('Обновление пакетов завершено')
-    except ValueError: 
-        print('Нет обновлений')
 
-th1 = Thread(target=dotter, args=())
-th2 = Thread(target=updater, args=())
-th1.start()
-th2.start()
+# def updater():
+#     global ulst
+    
+#     ulst = ulst.split()
+#     try:
+#         if ulst == []: raise ValueError('Пустое значение')
+#         for i in ulst[8::4]: subprocess.run(f'python -m pip install -U {i}'.split())
+#         print('Обновление пакетов завершено')
+#     except ValueError: 
+#         print('Нет обновлений')
+
 
 """
 Поиск обновлений \
