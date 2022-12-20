e1584127e0c7696168d77a771d3b65f753ad9687
AxeWar <50838140+evgensx@users.noreply.github.com>
2022-12-21 00:29:17 +0200

    Update pyupdater.py
    Removed unnecessary processes

--- pyupdater.py
+++ pyupdater.py
@@ -1,44 +1,27 @@
-import subprocess, time
-from threading import Thread
-from os import name, system
+import subprocess
+import time
+from os import system, name
 
-ulst = None
+def clear():
+    if name == 'nt': system('cls')  # windows
+    else: system('clear')           # mac linux
 
-def clear(): 
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
+proc = subprocess.Popen(args='python -m pip list -o'.split(), text=True, stdout=subprocess.PIPE)
 
-def progress():
-    global ulst
-    while True:
-        for i in ["\\", "|", "/", "-"]:
-            if ulst == None:
-                clear()
-                print(f'Поиск обновлений {i}', flush=True)
-                time.sleep(0.5)
-            else: 
-                exit()
-
-def updater():
-    global ulst
-    ulst = subprocess.run('python -m pip list -o'.split(), capture_output=True, text=True).stdout
+while proc.returncode == None:
+    for i in ["\\", "|", "/", "-"]:
+        proc.poll()
+        clear()
+        print(f'Поиск обновлений {i}')
+        time.sleep(0.5)
     clear()
-    if ulst != '':
-        ulst = ulst.split()
-        for i in ulst[8::4]: 
-            subprocess.run(f'python -m pip install -U {i}'.split())
-        print('Обновление пакетов завершено')
-    else:
-        print('Нет обновлений')
-    ulst = ulst.split()
 
-th1 = Thread(target=progress, args=())
-th2 = Thread(target=updater, args=())
+outs, _ = proc.communicate()
 
-th1.start()
-th2.start()
-
-th1.join()
-th2.join()
+if outs != '':
+    outs = outs.split()
+    for i in outs[8::4]:
+        subprocess.run(f'python -m pip install -U {i}'.split())
+    print('Обновление пакетов завершено')
+else:
+    print('Нет обновлений')
